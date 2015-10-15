d42cf98ed34d83a95c6ac79324c20adddd7095a8
Rylan Bonnevie <rylanb13@uw.edu>
2015-10-15 16:13:58 -0700

    finished part of Money

--- domain-modeling/main.swift
+++ domain-modeling/main.swift
@@ -1,6 +1,7 @@
-//
+//  Rylan Bonnevie
 //  main.swift
 //  domain-modeling
+//  Info 498 A
 //
 //  Created by Rylan Bonnevie on 10/15/15.
 //  Copyright © 2015 Rylan Bonnevie. All rights reserved.
@@ -8,5 +9,43 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Money {
+    var amount: Double = 10.0
+    var currency: String = "USD"
+    
+    func convert(currency: String, amount: Double, convertTo: String) -> Double {
+        var value: Double = amount;
+        if currency == "USD" {
+            if convertTo == "GBP" {
+                value = value / 2.0;
+            } else if convertTo == "EUR" {
+                value = value * 1.5;
+            } else {
+                value = value * 1.25;
+            }
+        } else if currency == "GBP" {
+            if convertTo == "USD" {
+                value = value * 2.0;
+            } else if convertTo == "EUR" {
+                value = value / 3
+            } else {
+                value = value / 2.5;
+            }
+        }
+        else if currency == "EUR" {
+            if convertTo == "GBP" {
+                value = value * 3.0;
+            } else if convertTo == "USD" {
+                value = value / 1.5;
+            } else {
+                value = value * 1.25;
+            }
+            
+        }
+        return value
+    }
+    func moneyOp (operation: String, money1: Money, money2: Money) -> Money {
+        if 
+    }
+}
 
