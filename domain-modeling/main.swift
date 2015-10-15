14b38681a9cfc79354cc622e1940dafafebf8ad3
Rylan Bonnevie <rylanb13@uw.edu>
2015-10-15 13:41:43 -0700

    Initial Commit

---
+++ domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Rylan Bonnevie on 10/15/15.
+//  Copyright © 2015 Rylan Bonnevie. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
