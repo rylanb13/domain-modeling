3f1c0c2dbd89faa0b8df092f00574995ed48479d
Rylan Bonnevie <rylanb13@uw.edu>
2015-10-19 01:07:50 -0700

    added money tests

--- domain-modeling/main.swift
+++ domain-modeling/main.swift
@@ -9,7 +9,23 @@
 
 import Foundation
 
-print(test2);
+func testChangeCurrency (thisCurrency: CurrencyType, changeType: CurrencyType) {
+    var testMoney = Money(value: 10.0, currency: thisCurrency);
+    testMoney.changeCurrency(changeType);
+    print("changed 10.0 \(thisCurrency) to \(testMoney.value) \(changeType)")
+}
 
+testChangeCurrency(CurrencyType.USD, changeType: CurrencyType.GBP)
+testChangeCurrency(CurrencyType.USD, changeType: CurrencyType.EUR)
+testChangeCurrency(CurrencyType.USD, changeType: CurrencyType.CAN)
+testChangeCurrency(CurrencyType.GBP, changeType: CurrencyType.USD)
+testChangeCurrency(CurrencyType.GBP, changeType: CurrencyType.EUR)
+testChangeCurrency(CurrencyType.GBP, changeType: CurrencyType.CAN)
+testChangeCurrency(CurrencyType.EUR, changeType: CurrencyType.GBP)
+testChangeCurrency(CurrencyType.EUR, changeType: CurrencyType.USD)
+testChangeCurrency(CurrencyType.EUR, changeType: CurrencyType.CAN)
+testChangeCurrency(CurrencyType.CAN, changeType: CurrencyType.GBP)
+testChangeCurrency(CurrencyType.CAN, changeType: CurrencyType.EUR)
+testChangeCurrency(CurrencyType.CAN, changeType: CurrencyType.USD)
 
 
