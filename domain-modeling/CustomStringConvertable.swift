7c11838ba0bc8c4f8946cafd8e9a64a235d9fd63
rylanb13 <rylanb13@uw.edu>
2015-10-20 19:35:10 -0700

    Delete CustomStringConvertable.swift

--- domain-modeling/CustomStringConvertable.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CustomStringConvertable.swift
-//  domain-modeling
-//
-//  Created by Rylan Bonnevie on 10/20/15.
-//  Copyright © 2015 Rylan Bonnevie. All rights reserved.
-//
-
-import Foundation
